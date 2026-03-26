2c2250f7f59ab16b8c355cb7944ecb127e966638
karan8877143 <karan.kumar135119@marwadiuniversity.ac.in>
2026-03-26 23:55:45 +0530

    Add files via upload

---
+++ if else.py
@@ -0,0 +1,44 @@
+a = int(input("Enter your age:,"))
+print("Your age is:", a)
+
+print(a>18)
+print(a==18)
+print(a!=18)    
+print(a<=18)    
+    
+
+if(a<=18):
+  print("you can use")
+
+else:
+  ("you cannot use")
+
+
+num =int(input("Enter your value of the num"))
+if(num<0):
+  print("number of negative.")
+elif(num==0):
+  print("number of zero.")
+else:
+  print("number of positive.")
+  
+
+
+num =20
+if(num<0):
+  print("number is negative")
+elif(num==0):
+    print("number is between 12-18")
+if(num<=10):
+    print("number is both 10")   
+else:
+    print("number is zero")
+
+
+num=30
+if(num<30):
+    print("number is positive")
+else:
+     print("number is nagetive")
+
+         
